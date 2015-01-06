21695c47575d4680bd7cc64e702ea75b65a73f28
lopixable <lopixable@gmx.com>
2015-01-06 19:18:12 +0100

    added: passing filename in parameter

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ pish
 
 PIng baSH
 
-bash script to ping multiple IP in less than 10 secondes
+Bash script to ping multiple IP in less than 10 secondes
 
 Use to ping AirVPN servers and choose the best
--- script.sh
+++ script.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 
-function GetIPByName {
-	egrep .*$1.* list.txt | cut -d: -f2
-}
-
-function GetNameByIP {
-	egrep .*$1.* list.txt | cut -d: -f1
-}
-
-function GetMSByName {
-	cat /tmp/ping-$1.out | egrep .*min/avg/max.* | cut -d"/" -f5
-}
-
-function LaunchPing {
-	for IP in $(cat list.txt | cut -d: -f2)
-	do
-		NAME=$(GetNameByIP $IP)
-		ping -qc 3 -W5 $IP > /tmp/ping-$NAME.out &
-	done
-}
-
-function getResult {
-	for NAME in $(cat list.txt | cut -d: -f1)
-	do
-		IP=$(GetIPByName $NAME)
-		IP="($IP)"
-		MS=$(GetMSByName $NAME)
-		printf "%-12s %-18s %8s\n" "$NAME" "$IP" "$MS"
-	done
-}
-
 echo "      _      _   "
 echo " ___ <_> ___| |_ "
 echo "| . \| |<_-<| . |"
 echo "|  _/|_|/__/|_|_|"
-echo "|_|          v1.0"
+echo "|_|          v1.1"
 echo 
+
+if [ -z $1 ]
+then
+    echo "Usage ./script.sh filename"
+    echo "Format of filename content: "
+    echo "Name:IPv4"
+    exit 1
+elif [ ! -f $1 ]
+then
+    echo "$1 is not a valid file"
+    exit 2
+fi
+
+FILE=$1
+
+function GetIPByName {
+    egrep .*$1.* $FILE | cut -d: -f2
+}
+
+function GetNameByIP {
+    egrep .*$1.* $FILE | cut -d: -f1
+}
+
+function GetMSByName {
+    cat /tmp/ping-$1.out | egrep .*min/avg/max.* | cut -d"/" -f5
+}
+
+function LaunchPing {
+    for IP in $(cat $FILE | cut -d: -f2)
+        do
+            NAME=$(GetNameByIP $IP)
+                ping -qc 3 -W5 $IP > /tmp/ping-$NAME.out &
+                done
+}
+
+function getResult {
+    for NAME in $(cat $FILE | cut -d: -f1)
+        do
+            IP=$(GetIPByName $NAME)
+                IP="($IP)"
+                MS=$(GetMSByName $NAME)
+                printf "%-12s %-18s %8s\n" "$NAME" "$IP" "$MS"
+                done
+}
+
 LaunchPing
 echo "Pings in progress..."
 sleep 7
